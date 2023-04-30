9088287e3d4b6e561bee7134481f1ed436fffefd
Jake Leavitt <jake.leavitt74@gmail.com>
2023-04-30 16:02:58 -0400

    pcaDta

--- FinalProject.py
+++ FinalProject.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -8,6 +9,21 @@ def main():
     data = data.astype({"id":"category","hasYard":"category","hasPool":"category","cityPartRange":"category","isNewBuilt":"category","hasStormProtector":"category","hasStorageRoom":"category"})
     data = data.drop(["id", "cityCode"], axis=1)
 
+    # print(data.columns)
+
+    pca = PCA(n_components=2)
+
+    pcaData = data.to_numpy()[:, :-1]
+
+    pca.fit(pcaData)
+
+    print(pca.explained_variance_ratio_)
+
+    pcaT = pca.transform(pcaData)
+
+    plt.scatter(pcaData[:, 0], pcaData[:, 1])
+
+    plt.show()
 
 
 if __name__ == "__main__":
