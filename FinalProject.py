33a49b4f9849dac8aa6ffe5a81d9a845cb2effba
Jake Leavitt <jake.leavitt74@gmail.com>
2023-04-30 15:52:36 -0400

    data

--- FinalProject.py
+++ FinalProject.py
@@ -1,3 +1,14 @@
+import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
 
+
+def main():
+    data = pd.read_csv("train.csv")
+    data = data.astype({"id":"category","hasYard":"category","hasPool":"category","cityPartRange":"category","isNewBuilt":"category","hasStormProtector":"category","hasStorageRoom":"category"})
+    data = data.drop(["id", "cityCode"], axis=1)
+
+
+
+if __name__ == "__main__":
+    main()
